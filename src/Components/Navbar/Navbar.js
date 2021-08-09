ee98730f79e59339c752517420ee3cb39a8ea2c9
patsyguerrero <patsy.guerrero95@gmail.com>
2021-08-09 08:31:00 -0500

    responsive

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import { HiChevronDown } from "react-icons/hi";
 
 const navigation = [
   { name: 'Cundinamarca', href: '#', current: true },
@@ -15,14 +16,14 @@ function classNames(...classes) {
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-white">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 bg-white rounded-md text-gray-700 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -34,13 +35,13 @@ export default function Navbar() {
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
                   <img
-                    className="block lg:hidden h-8 w-auto"
-                    src={require(`../../assets/Group.jpg`).default}
+                    className="block h-4 w-auto"
+                    src={require('../../assets/Vector.svg').default}
                     alt="Workflow"
                   />
                   <img
-                    className="hidden lg:block h-8 w-auto"
-                    src={require('../../assets/icon.png')}
+                    className="hidden lg:block h-4 w-auto"
+                    src={require('../../assets/iconhaus.svg').default}
                     alt="Workflow"
                   />
                 </div>
@@ -51,7 +52,7 @@ export default function Navbar() {
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          item.current ? 'bg-gray-100 text-gray-700' : 'text-gray-700 hover:bg-gray-100 hover:text-gray-700',
                           'px-3 py-2 rounded-md text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -69,13 +70,11 @@ export default function Navbar() {
                   {({ open }) => (
                     <>
                       <div>
-                        <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                        <Menu.Button className="bg-gray-100 flex text-gray-700 text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                          Mi perfil
+                          <HiChevronDown/>
                           <span className="sr-only">Open user menu</span>
-                          <img
-                            className="h-8 w-8 rounded-full"
-                            src=""
-                            alt=""
-                          />
+                      
                         </Menu.Button>
                       </div>
                       <Transition
@@ -110,7 +109,7 @@ export default function Navbar() {
                               <a
                                 href="#"
                                 className={classNames(
-                                  active ? 'bg-gray-100' : '',
+                                  active ? 'bg-gray-700' : '',
                                   'block px-4 py-2 text-sm text-gray-700'
                                 )}
                               >
@@ -121,9 +120,9 @@ export default function Navbar() {
                           <Menu.Item>
                             {({ active }) => (
                               <a
-                                href="#"
+                                href="#p"
                                 className={classNames(
-                                  active ? 'bg-gray-100' : '',
+                                  active ? 'bg-gray-700' : '',
                                   'block px-4 py-2 text-sm text-gray-700'
                                 )}
                               >
@@ -147,7 +146,7 @@ export default function Navbar() {
                   key={item.name}
                   href={item.href}
                   className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    item.current ? 'bg-gray-200 text-black' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                     'block px-3 py-2 rounded-md text-base font-medium'
                   )}
                   aria-current={item.current ? 'page' : undefined}
--- src/Container/ContainerCard.js
+++ src/Container/ContainerCard.js
@@ -6,7 +6,7 @@ export default function ContainerCard() {
     let included= data.included
     console.log('data',data)
     return (
-        <div className='grid justify-items-center grid-cols-3 gap-12 auto-cols-auto mt-8' >
+        <div className='grid  grid-cols-1 sm:justify-items-center sm:grid-cols-3 gap-12 sm:auto-cols-auto mt-8' >
             {data.data.map(card => (
                 <Card attributes={card.attributes} included={included}/>
             ))
